90105dcd7fc3f6d6929b901df5c745638b20c410
Dylan McConnell <dylan-climate-energy-college@users.noreply.github.com>
2019-01-04 19:05:00 +1100

    Added sql engine to __init__

--- __init__.py
+++ __init__.py
@@ -1,7 +1,11 @@
-"""initialises config file"""
+"""initialises config data and sql engine"""
 import os
 import configparser
+from sqlalchemy import create_engine
+
 MODULE_DIR = os.path.dirname(__file__)
 
 CONFIG = configparser.RawConfigParser()
 CONFIG.read(os.path.join(MODULE_DIR, 'config.ini'))
+
+ENGINE = create_engine("sqlite:///{0}".format(os.path.join(CONFIG["local"]["db_path"], "test.db")))
