6ac7da36dd7dc1d6e833684b1d40b9d58e8edda7
Dylan McConnell <dylan-climate-energy-college@users.noreply.github.com>
2019-01-04 11:23:18 +1100

    Linting and commenting

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,14 +1,17 @@
-import pytest
+"""simple test to ensure example_config.ini reflects actual config.ini used"""
 import os
 import configparser
+import pytest
 from cemo_outputs import CONFIG, MODULE_DIR
 
-test_config = configparser.RawConfigParser()
-test_config.read(os.path.join(MODULE_DIR,'example_config.ini'))
+TEST_CONFIG = configparser.RawConfigParser()
+TEST_CONFIG.read(os.path.join(MODULE_DIR, 'example_config.ini'))
 
 def test_config_sections():
-    assert CONFIG.sections() == test_config.sections()
+    """checks config sections are the same"""
+    assert CONFIG.sections() == TEST_CONFIG.sections()
 
 @pytest.mark.parametrize("section", CONFIG.sections())
 def test_section_keys(section):
-    assert test_config[section].keys() == CONFIG[section].keys()
+    """checks keys in each sections are the same"""
+    assert TEST_CONFIG[section].keys() == CONFIG[section].keys()
