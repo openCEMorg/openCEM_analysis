da617ab8ce811a32ea71d86b786d73157580cac8
Dylan McConnell <dylan-climate-energy-college@users.noreply.github.com>
2019-01-04 19:41:37 +1100

    linting, docstrings etc

--- json_parser.py
+++ json_parser.py
@@ -1,3 +1,4 @@
+"""Module for parsing json file output, and storing in SQL db"""
 import os
 from collections import namedtuple
 import json
@@ -5,6 +6,9 @@ import pandas as pd
 from cemo_outputs import CONFIG, ENGINE
 
 class CemoJsonFile(object):
+    """CEMO JSON File class
+    Loads JSON file and contains method to process the yearly data (and meta data in the
+    future)"""
     def __init__(self, filename="ISP_N_cp_sol.json"):
         self.filename = filename
         self.load_json()
@@ -17,32 +21,40 @@ class CemoJsonFile(object):
             self.json = json.load(_file)
 
     def process_years(self):
+        """Process each year of data contained in the JSON file"""
         for year in self.meta['Years']:
             year_data = YearData(year)
             year_data.load_data(self)
             year_data.process_vars()
 
 class YearData(object):
+    """Class to process datasets from each year of output"""
     def __init__(self, year="2020"):
         self.year = year
         self.data = None
         self.objective_value = None
 
     def load_data(self, cemo_json_file):
+        """Method that updates the data attribute. Maybe uncessessary"""
         self.data = cemo_json_file.json[str(self.year)]
-        self.objective_value = self.data['objective_value']
 
     def process_vars(self):
-        for name, variable_map, in VARIABLES.items():
+        """Method to process each variable dataset contained in VARIABLES """
+        for _, variable_map, in VARIABLES.items():
             dataset = VariableDataset(variable_map)
             dataset.parse_all(self)
             dataset.insert_dataset()
 
 class VariableDataset(object):
+    """ Class to parse and insert the data from a particular dataset into an SQL db"""
     def __init__(self, variable_map):
         self.variable_map = variable_map
+        self.dataframe = None
 
     def parse_var(self, yeardata, var):
+        """Converts raw list (from json file) containing `index` and `value` items into
+        formatted list more suitable for creating a pandas dataframe. Uses this to formatted list
+        to generate and return dataframe"""
         raw_list = yeardata.data['vars'][var]
         formated_list = [i['index']+[i['value']] for i in raw_list]
         dataframe = pd.DataFrame(formated_list)
@@ -50,17 +62,22 @@ class VariableDataset(object):
         return dataframe
 
     def parse_all(self, yeardata):
+        """Generates and concatenates all the data frames for dataset into a single data frame"""
         series_list = [self.parse_var(yeardata, var) for var in self.variable_map.variable_list]
-        self.df = pd.concat(series_list)
-        
+        self.dataframe = pd.concat(series_list)
+
     def insert_dataset(self, engine=ENGINE):
-        self.df.to_sql(self.variable_map.dataset_name, con=engine, if_exists="append", index=None)
+        """Method in insert parsed and concatenated data into SQL db with ENGINE"""
+        self.dataframe.to_sql(self.variable_map.dataset_name,
+                              con=engine,
+                              if_exists="append",
+                              index=None)
 
 #  class factory function for variable dataset metadata
 VariableMap = namedtuple("VariableMap",
-                            ["dataset_name",
-                             "columns",
-                             "variable_list"])
+                         ["dataset_name",
+                          "columns",
+                          "variable_list"])
 
 VARIABLES = {
     "generation" : VariableMap(
@@ -74,7 +91,7 @@ VARIABLES = {
     "storage_level" : VariableMap(
         dataset_name="storage_level",
         columns=['ntndp_zone_id', 'technology_type_id', 'timestamp', 'value'],
-        variable_list=["stor_level", "hyb_level"]),                       
+        variable_list=["stor_level", "hyb_level"]),
     "energy_balance" : VariableMap(
         dataset_name="energy_balance",
         columns=['region_id', 'timestamp', 'value'],
