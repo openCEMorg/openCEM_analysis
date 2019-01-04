b4cc28622a4fa2e263228c2c8813379de7d16482
Dylan McConnell <dylan-climate-energy-college@users.noreply.github.com>
2019-01-04 11:20:08 +1100

    Corrected import path, fixed vars

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,14 +1,14 @@
 import pytest
 import os
 import configparser
-from opennem import config, module_dir
+from cemo_outputs import CONFIG, MODULE_DIR
 
 test_config = configparser.RawConfigParser()
-test_config.read(os.path.join(module_dir,'sample_config.ini'))
+test_config.read(os.path.join(MODULE_DIR,'example_config.ini'))
 
 def test_config_sections():
-    assert config.sections() == test_config.sections()
+    assert CONFIG.sections() == test_config.sections()
 
-@pytest.mark.parametrize("section", config.sections())
+@pytest.mark.parametrize("section", CONFIG.sections())
 def test_section_keys(section):
-    assert test_config[section].keys() == config[section].keys()
+    assert test_config[section].keys() == CONFIG[section].keys()
