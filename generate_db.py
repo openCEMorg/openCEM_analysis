cd05a522db2d9306305975e5ce78d8562b7095a3
José Ignacio Zapata Fuentealba <jose.zapata@itpau.com.au>
2019-04-11 12:30:59 +1000

    pylint for generate_db

--- generate_db.py
+++ generate_db.py
@@ -1,10 +1,13 @@
+'''Generate sqlite3 file from json file for faster processing of data'''
+
 import sys
 
 from json_sqlite import CONFIG, json_parser
+
+# First generate db file from json file
+# point to the directory holding the cemo_outputs folder
 JSON_PATH = CONFIG['local']['json_path']
 sys.path.append(JSON_PATH)
 JSON_NAME = CONFIG['local']['json_name']
 DATA = json_parser.CemoJsonFile(JSON_NAME)
 DATA.process_years()
-
-### Then generate report from db file ###
