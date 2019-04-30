462757445d36ef940038e0ed716c9f92146c2635
Dylan McConnell <dylan-climate-energy-college@users.noreply.github.com>
2019-04-30 15:51:35 +1000

    Added mysql engines for creating and inserting output data

--- __init__.py
+++ __init__.py
@@ -9,3 +9,5 @@ CONFIG = configparser.RawConfigParser()
 CONFIG.read(os.path.join(MODULE_DIR, 'config.ini'))
 
 ENGINE = create_engine("sqlite:///{0}".format(CONFIG["local"]["db_path"]))
+MYSQL_INSERT = create_engine("mysql://{python_name}:{python_psswd}@{hostname}/opencem_output?unix_socket={socket}".format(**CONFIG['local_mysql']))
+MYSQL_CREATE = create_engine("mysql://root:{root_psswd}@{hostname}/opencem_output?unix_socket={socket}".format(**CONFIG['local_mysql']))
