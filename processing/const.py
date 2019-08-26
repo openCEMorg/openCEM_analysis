fa5810687e0d76b72bf671f02be5b797154e148d
José Ignacio Zapata Fuentealba <jose.zapata@itpau.com.au>
2019-08-26 16:26:42 +1000

    Updated pallette and tech names to be in line with openCEM website

--- processing/const.py
+++ processing/const.py
@@ -8,102 +8,70 @@ __maintainer__ = "José Zapata"
 __email__ = "jose.zapata@itpau.com.au"
 __status__ = "Development"
 
-PALETTE = {1: (161 / 255, 135 / 255, 111 / 255, 1),  # biomass
-           2: (251 / 255, 177 / 255, 98 / 255, 1),  # ccgt
-           3: (251 / 255, 177 / 255, 98 / 255, 0.75),  # ccgt_sc
-           4: (25 / 255, 25 / 255, 25 / 255, 1),  # coal_sc
-           5: (25 / 255, 25 / 255, 25 / 255, 0.75),  # coal_sc_scc
-           6: (137 / 255, 87 / 255, 45 / 255, 1),  # brown_coal_sc
-           7: (137 / 255, 87 / 255, 45 / 255, 0.75),   # brown_coal_sc_scc
-           8: (253 / 255, 203 / 255, 148 / 255, 1),  # ocgt
-           9: (220 / 255, 205 / 255, 0, 0.6),  # PV DAT
-           10: (220 / 255, 205 / 255, 0 / 255, 0.8),  # PV fixed
-           11: (220 / 255, 205 / 255, 0 / 255, 1),  # PV SAT
-           12: (67 / 255, 116 / 255, 14 / 255, 1),  # Wind
-           13: (1, 209 / 255, 26 / 255, 1),  # CST 6h
-           14: (137 / 255, 174 / 255, 207 / 255, 1),  # PHES 6 h
-           15: (43 / 255, 161 / 255, 250 / 255, 1),  # Battery
-           16: (240 / 255, 79 / 255, 35 / 255, 1),  # recip engine,
-           17: (52 / 255, 76 / 255, 55 / 255, 1), #(128 / 255, 191 / 255, 1, 1),  # Wind high
-           18: (75 / 255, 130 / 255, 178 / 255, 1),  # Hydro
-           19: (241 / 255, 140 / 255, 31 / 255, 1),  # Gas thermal
-           20: (0 / 255, 96 / 255, 1, 1),  # pumps
-           21: (162 / 255, 47 / 255, 47 / 255, 1),  # load (red)
-           22: (140 / 255, 140 / 255, 140 / 255, 1),  # Light gray other tech 1
-           23: (145 / 255, 145 / 255, 145 / 255, 1),  # Light gray other tech 2
-           24: (150 / 255, 150 / 255, 150 / 255, 1),  # Light gray other tech 3
-           25: (160 / 255, 160 / 255, 160 / 255, 1),  # Light gray other tech 4
-           }
-PALETTE_2 = {1: (161 / 255, 135 / 255, 111 / 255, 1),  # biomass
-             2: (251 / 255, 177 / 255, 98 / 255, 1),  # ccgt
-             3: (251 / 255, 177 / 255, 98 / 255, 0.75),  # ccgt_sc
-             4: (25 / 255, 25 / 255, 25 / 255, 1),  # coal_sc
-             5: (25 / 255, 25 / 255, 25 / 255, 0.75),  # coal_sc_scc
-             6: (137 / 255, 87 / 255, 45 / 255, 1),  # brown_coal_sc
-             7: (137 / 255, 87 / 255, 45 / 255, 0.75),   # brown_coal_sc_scc
-             8: (253 / 255, 203 / 255, 148 / 255, 1),  # ocgt
-             9: (220 / 255, 205 / 255, 0, 0.6),  # PV DAT
-             10: (220 / 255, 205 / 255, 0 / 255, 0.8),  # PV fixed
-             11: (220 / 255, 205 / 255, 0 / 255, 1),  # PV SAT
-             12: (67 / 255, 116 / 255, 14 / 255, 1),  # Wind
-             13: (1, 209 / 255, 26 / 255, 1),  # CST 6h
-             14: (137 / 255, 174 / 255, 207 / 255, 1),  # PHES 6 h
-             15: (43 / 255, 161 / 255, 250 / 255, 1),  # Battery
-             16: (240 / 255, 79 / 255, 35 / 255, 1),  # recip engine,
-             17: (52 / 255, 76 / 255, 55 / 255, 1), #(128 / 255, 191 / 255, 1, 1),  # Wind high
-             18: (75 / 255, 130 / 255, 178 / 255, 1),  # Hydro
-             19: (241 / 255, 140 / 255, 31 / 255, 1),  # Gas thermal
-             20: (0 / 255, 96 / 255, 1, 1),  # pumps
-             21: (1 / 255, 50 / 255, 67 / 255, 1),  # Snowy2.0
-             22: (140 / 255, 140 / 255, 140 / 255, 1),  # Light gray other tech 1
-             23: (145 / 255, 145 / 255, 145 / 255, 1),  # Light gray other tech 2
-             24: (150 / 255, 150 / 255, 150 / 255, 1),  # Light gray other tech 3
-             25: (160 / 255, 160 / 255, 160 / 255, 1),  # Light gray other tech 4
-             }
-TECH_NAMES = {1:'Biomass',
-              2:'CCGT',
-              3:'CCGT CCS',
-              4:'Coal SC',
-              5:'Coal SC CCS',
-              6:'Brown Coal SC',
-              7:'Brown Coal SC CCS',
-              8:'OCGT',
-              9:'Solar PV DAT',
-              10:'Solar PV FFP',
-              11:'Solar PV SAT',
-              12:'Wind',
-              13:'CST 6h',
-              14:'PHES 6h',
-              15:'Battery 2h',
-              16:'Recip Engine',
-              17:'Wind (High)',
-              18:'Hydro',
-              19:'Gas (Thermal)',
-              20:'Pumps',
-              21:'Snowy 2.0'
-              }
-TECH_W_LOAD = {1:'Biomass',
-               2:'CCGT',
-               3:'CCGT CCS',
-               4:'Coal SC',
-               5:'Coal SC CCS',
-               6:'Brown Coal SC',
-               7:'Brown Coal SC CCS',
-               8:'OCGT',
-               9:'Solar PV DAT',
-               10:'Solar PV FFP',
-               11:'Solar PV SAT',
-               12:'Wind (High)',
-               13:'CST 6h',
-               14:'PHES 6h',
-               15:'Battery 2h',
-               16:'Recip Engine',
-               17:'Wind H',
-               18:'Hydro',
-               19:'Gas (Thermal)',
-               20:'Pumps',
-               21:'Load'
-               }
+PALETTE = {
+    1: '#A3886F',  # biomass
+    2: '#F9DCBC',  # ccgt
+    3: '#FFCD96',  # ccgt_CCS
+    4: '#080808',  # coal_sc
+    5: '#4C4C4C',  # coal_sc_CCS
+    6: '#8B572A',  # brown_coal
+    7: '#957559',  # brown_coal_sc_CCS
+    8: '#FDB363',  # ocgt
+    9: '#A3980F',  # PV DAT
+    10: '#D8C809',  # PV fixed
+    11: '#FFF25C',  # PV SAT
+    12: '#508313',  # Wind
+    13: '#ecc704',  # CST 6h
+    14: '#094F87',  # PHES 6 h
+    15: '#EEEEEE',  # Battery
+    16: '#F35020',  # recip engine,
+    17: '#427505',  # Wind high
+    18: '#4482B4',  # Hydro
+    19: '#F48E1C',  # Gas thermal
+    20: '#C5BEBE',  # pumps
+    21: '#091587',  # load (red)
+    22: '#e1bd03',  # Light gray other tech 1
+    23: '#f7d004',  # Light gray other tech 2
+    24: '#195F97',  # Light gray other tech 3
+    25: '#004878',  # Light gray other tech 4
+    26: '#F0F0F0',
+    27: '#FFFFFF',
+    28: '#282828'
+}
+PALETTE_2 = PALETTE
+
+TECH_NAMES = {
+    1: 'Biomass',
+    2: 'CCGT',
+    3: 'CCGT CCS',
+    4: 'Coal SC',
+    5: 'Coal SC CCS',
+    6: 'Brown Coal SC',
+    7: 'Brown Coal SC CCS',
+    8: 'OCGT',
+    9: 'Solar PV DAT',
+    10: 'Solar PV FFP',
+    11: 'Solar PV SAT',
+    12: 'Wind (low)',
+    13: 'CST 6h',
+    14: 'PHES 6h',
+    15: 'Battery 2h',
+    16: 'Recip Engine',
+    17: 'Wind (High)',
+    18: 'Hydro',
+    19: 'Gas (Thermal)',
+    20: 'Pumps',
+    21: 'Snowy 2.0',
+    22: 'CST 3h',
+    23: 'CST 12h',
+    24: 'PHES 3h',
+    25: 'PHES 12h',
+    26: 'Battery 1h',
+    27: 'Battery 3h',
+    28: 'Black Coal (New)'
+}
+TECH_W_LOAD = TECH_NAMES  # REVIEW
+
 REGIONS = ["NSW", "QLD", "SA", "TAS", "VIC"]
 REGION = {1: 'NSW', 2: 'QLD', 3: 'SA', 4: 'TAS', 5: 'VIC'}
 ZONE = {
@@ -125,5 +93,7 @@ ZONE = {
     16: 'TAS'
 }
 
-DISPLAY_ORDER = [6, 7, 4, 5, 1, 16, 19, 2, 3, 8, 15, 18, 14, 17, 12, 13, 9, 10, \
-                11, 20, 21, 22, 23, 24, 25]
+DISPLAY_ORDER = [
+    6, 7, 4, 5, 1, 16, 19, 2, 3, 8, 24, 14, 25, 21, 18, 26, 15, 27, 12, 17, 22,
+    13, 23, 9, 10, 11
+]
